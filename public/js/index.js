283b615cf9075c504462a4c03e1c04f8ccbd6198
Brian Rose <rose.bri@husky.neu.edu>
2015-04-14 00:42:10 -0400

    added forgotten files

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+﻿var app = angular.module("FinalApp", ["ngRoute"]);
+
+app.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+        when('/home', {
+            templateUrl: 'pages/home.html'
+        }).
+        when('/profile', {
+            templateUrl: 'pages/profile.html'
+        }).
+        when('/login', {
+            templateUrl: 'pages/login.html'
+        }).
+        when('/register', {
+            templateUrl: 'pages/register.html'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+    }
+]);
