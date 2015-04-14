a55eedbec02670cc081814bd600e3861d27b4c3e
Brian Rose <rose.bri@husky.neu.edu>
2015-04-14 00:20:54 -0400

    added files

---
+++ public/js/profile.js
@@ -0,0 +1,60 @@
+﻿var app = angular.module("OnlineUniversity", []);
+
+var dialog;
+
+app.controller("OnlineUniversityController", function ($scope, $http) {
+    $http.get('/api/course')
+    .success(function (response) {
+        $scope.courses = response;
+    });
+
+    $scope.remove = function (index) {
+        $http.delete('/api/course/' + index)
+        .success(function (response) {
+            $scope.courses = response;
+        });
+    };
+
+    dialog = $("#dialog");
+
+    emptyDialog = dialog.clone();
+
+    $scope.selectedIndex = null;
+
+    $scope.update = function (index) {
+        $scope.newCourse = $scope.courses[index];
+        dialog.modal('show');
+        $scope.selectedIndex = index;
+    }
+
+    $scope.newCourseClick = function () {
+        //$scope.newCourse = [];
+        dialog.modal('show');
+        $scope.selectedIndex = -1;
+    }
+
+
+    $scope.add = function (newCourse) {
+        console.log(newCourse);
+        console.log($scope.selectedIndex);
+
+        if ($scope.selectedIndex == -1) {
+            $http.post('/api/course', newCourse)
+            .success(function (response) {
+                $scope.courses = response;
+            });
+        }
+        else {
+            $http.put('/api/course/' + $scope.selectedIndex, newCourse)
+            .success(function (response) {
+                $scope.courses = response;
+            });
+        }
+
+        dialog.modal('hide');
+    }
+
+    $scope.close = function () {
+        dialog.modal('hide');
+    }
+});
