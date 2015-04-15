2d296e6689447bbfee84aee7d8b5986fab824e79
Brian Rose <rose.bri@husky.neu.edu>
2015-04-14 21:44:40 -0400

    Added mongodb and a getter for teams

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var app = express();
+
+var TeamSchema = new mongoose.Schema({
+    city: String,
+    nickname: String,
+    created: { type: Date, default: Date.now }
+}, { collection: 'team' });
+
+var TeamModel = mongoose.model('TeamModel', TeamSchema);
+
+app.get('/api/team', function (req, res) {
+    TeamModel.find(function (err, teams) {
+        res.json(teams);
+    });
+});
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
