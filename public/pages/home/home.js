ed3a149bd32f2a2ae46c14b301d21d417dbe8d8c
Brian Rose <rose.bri@husky.neu.edu>
2015-04-16 02:06:27 -0400

    Re-organized files

---
+++ public/pages/home/home.js
@@ -0,0 +1,9 @@
+﻿app.controller('HomeController', function ($scope, $http) {
+    $scope.search = function (searchTerm) {
+        $http.get('/api/team/' + searchTerm)
+        .success(function (response) {
+            console.log(response);
+            $scope.teams = response;
+        });
+    }
+});
