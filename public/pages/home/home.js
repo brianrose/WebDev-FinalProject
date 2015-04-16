ad7111df2428c012c420585f688680208cbf7256
Brian Rose <rose.bri@husky.neu.edu>
2015-04-16 13:18:44 -0400

    Update

--- public/pages/home/home.js
+++ public/pages/home/home.js
@@ -7,6 +7,21 @@
         });
     }
 
+    $scope.favoriteTeams = [];
+    console.log("Get favorite teams");
+    console.log($rootScope.currentUser._id);
+    $http.get('/api/favoriteTeams/' + $rootScope.currentUser._id)
+    .success(function (response) {
+        console.log("Favorite teams");
+        console.log(response);
+        for (var i = 0; i < response.length; i++) {
+            $http.get('/api/team/id/' + response[i])
+            .success(function (response) {
+                $scope.favoriteTeams.push(response);
+            });
+        }
+    });
+
     $scope.addToFavoriteTeams = function (team) {
         console.log("Add to favorite teams");
         console.log($rootScope.currentUser);
