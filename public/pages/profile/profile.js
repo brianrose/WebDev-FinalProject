87f9ad0f99ccebeec8957d71695ce63eabeb8359
Brian Rose <rose.bri@husky.neu.edu>
2015-04-16 12:07:54 -0400

    Fix favorit teams

--- public/pages/profile/profile.js
+++ public/pages/profile/profile.js
@@ -3,7 +3,13 @@
     for (var i = 0; i < $rootScope.currentUser.favoriteTeams.length; i++) {
         $http.get('/api/team/id/' + $rootScope.currentUser.favoriteTeams[i])
         .success(function (response) {
+            console.log("get a team");
+            console.log(response);
             $scope.favoriteTeams.push(response);
         });
+    }
+
+    $scope.removeFromFavoriteTeams = function (team) {
+        //TODO: http.delete
     }
 });
